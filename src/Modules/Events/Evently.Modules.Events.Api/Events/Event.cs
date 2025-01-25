90d017c367785bd448b2d154db9cd2a094405a70
gajalves <ga.jaimealves@gmail.com>
2025-01-25 20:10:11 -0300

    first commit

---
+++ src/Modules/Events/Evently.Modules.Events.Api/Events/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace Evently.Modules.Events.Api.Events;
+public sealed class Event
+{
+    public  Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime StartAtUtc { get; set; }
+    public DateTime? EndsAtUtc { get; set; }
+    public EventStatus Status { get; set; }
+}
