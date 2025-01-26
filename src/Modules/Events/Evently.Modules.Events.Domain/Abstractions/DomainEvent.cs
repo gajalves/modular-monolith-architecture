86f849e1dbbeb13afedf2c891c5b2f2fdbb5240d
gajalves <ga.jaimealves@gmail.com>
2025-01-26 18:06:31 -0300

    Refactoring to Clean Architecture

---
+++ src/Modules/Events/Evently.Modules.Events.Domain/Abstractions/DomainEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Evently.Modules.Events.Domain.Abstractions;
+
+public abstract class DomainEvent : IDomainEvent
+{
+    protected DomainEvent()
+    {
+        Id = Guid.NewGuid();
+        OccurredOnUtc = DateTime.UtcNow;
+    }
+
+    public Guid Id { get; init; }
+
+    public DateTime OccurredOnUtc { get; init; }
+
+    protected DomainEvent(Guid id, DateTime occurredOnUtc)
+    {
+        Id = id;
+        OccurredOnUtc = occurredOnUtc;
+    }
+}
---
+++ src/Modules/Events/Evently.Modules.Events.Domain/Abstractions/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace Evently.Modules.Events.Domain.Abstractions;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    protected Entity() { }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.ToList();
+
+    protected void Raise(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
